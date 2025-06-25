2a21a8ed49f8263067227102c2fb7d7e763e93c1
Mathu-Mathi-M <mathu9147@gmail.com>
2025-06-25 10:51:11 +0530

    Initial commit

---
+++ lib/mongodb.ts
@@ -0,0 +1,34 @@
+// lib/mongodb.ts
+import { MongoClient, type Db, MongoClientOptions } from "mongodb";
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb+srv://foodrecipe:foodrecipe@chefora.ira8gvx.mongodb.net/";
+
+// For Atlas, you should NOT allow invalid certificates!
+const MONGODB_OPTIONS: MongoClientOptions = {
+  tls: true,
+  tlsAllowInvalidCertificates: false, // This must be false for Atlas!
+};
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+let cachedClient: MongoClient | null = null;
+let cachedDb: Db | null = null;
+
+export async function connectToDatabase() {
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb };
+  }
+
+  const client = new MongoClient(MONGODB_URI, MONGODB_OPTIONS);
+  await client.connect();
+
+  const db = client.db("chefora");
+
+  cachedClient = client;
+  cachedDb = db;
+
+  return { client, db };
+}
